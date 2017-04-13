ba8bda8ab951447af47b392a8d278bfc31b2ffb3
pleech3 <pleech3@gatech.edu>
2017-04-12 21:00:22 -0500

    Provides DAO and implementation

---
+++ Phase3/team073a/src/main/java/edu/gatech/csedbs/team073/dao/ProvideDAO.java
@@ -0,0 +1,13 @@
+package edu.gatech.csedbs.team073.dao;
+
+import edu.gatech.csedbs.team073.model.Provide;
+
+/**
+ * Created by Phil on 4/12/2017.
+ */
+public interface ProvideDAO {
+
+
+    public Provide getProvideBySiteId(int site_id);
+
+}
